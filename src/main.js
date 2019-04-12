1ffe890eefef312b68494b96a737941b2685c429
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-12 11:06:50 +0300

    Connect db

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import firebaseConfig from './config/firebase'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+Vue.$db = db
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+
+  created() {
+    //load book
+  }
 }).$mount('#app')
