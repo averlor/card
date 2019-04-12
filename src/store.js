fc2d530311121b8686eb6cf2308b223247a2057a
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-12 11:10:55 +0300

    Edit store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import generalModule from './store/general.js'
+import booksModule from './store/books.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    generalModule,
+    booksModule
   }
 })
