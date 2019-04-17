cebad2a40ac311eb0287ae5197b82137c86fc11d
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-17 13:10:48 +0300

    Edit main page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <BooksList />
+       <v-container>
+           <Header />
+        </v-container>
+        <v-container>
+            <BooksList />
+        </v-container>
     </div>
 </template>
 
 <script>
 
 import BooksList from '@/components/BooksList.vue'
+import Header from '@/components/AppHeader.vue'
 
 export default {
     name: 'books',
     components: {
-        BooksList
+        BooksList,
+        Header
     }
 }
 </script>
