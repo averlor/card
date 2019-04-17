72b3b7ede39986f8720518c32f1f91f8ae78c6b9
Vladimir Abramyants <vabramanc@gmaill.com>
2019-04-17 13:10:18 +0300

    edit main store: added new module -> cart.js

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 
 import generalModule from './store/general.js'
 import booksModule from './store/books.js'
+import cartModule from './store/cart.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     generalModule,
-    booksModule
+    booksModule,
+    cartModule
   }
 })
